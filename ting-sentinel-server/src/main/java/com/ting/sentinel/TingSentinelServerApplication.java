7662e698a7ac04dfc40e3b0e2cc1f492c2054888
clannad <574785182@qq.com>
2019-12-29 23:02:55 +0800

    sentinel init

--- ting-sentinel-server/src/main/java/com/ting/sentinel/TingSentinelServerApplication.java
+++ ting-sentinel-server/src/main/java/com/ting/sentinel/TingSentinelServerApplication.java
@@ -1,15 +1,29 @@
 package com.ting.sentinel;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class TingSentinelServerApplication  {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(TingSentinelServerApplication.class, args);
 	}
+
+	@RestController
+	public class TestController {
+
+		@GetMapping(value = "/hello")
+		@SentinelResource("hello")
+		public String hello() {
+			return "Hello Sentinel";
+		}
+
+	}
+
 
 }
